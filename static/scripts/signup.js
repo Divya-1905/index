b8d6fb8c5457668479ee82e23260961d427828ff
Divya-1905 <divyasri1905@gmail.com>
2022-12-30 18:18:29 +0530

    update

--- static/scripts/signup.js
+++ static/scripts/signup.js
@@ -2,15 +2,15 @@ var host = window.location.protocol+" //"+ window.location.host
 var name,email,address,phone 
 let csrf_token = document.querySelector(['namemilddwaretoken']).value
 function get(){
-    var name = document.getElementById('id_name').value
-    var email = document.getElementById("id_email").value
-    var addres = document.getElementById("id_address").value
-    var phone = document.getElementById("id_phone").value
+     name = document.getElementById('id_name').value
+     email = document.getElementById("id_email").value
+     address = document.getElementById("id_address").value
+     phone = document.getElementById("id_phone").value
 }
 console.log(email,address,phone, name )
 fetch('http://127.0.0.1:8000/accounts/signup', {
     method: 'POST',
-    body: JSON.stringify({'email':email,'phone':phone,'name':name}),
+    body: JSON.stringify({'email':email,'phone':phone,'name':name,'address':address}),
     headers: new Headers({
       'X-CSRFToken':csrf_token,
       'Content-Type': 'application/json'
